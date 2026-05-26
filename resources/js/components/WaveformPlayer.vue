ab26054ee486104c9bc6d4b4598e1d2685679489
Lorenzo Minciotti <lorenzo.minciotti@novanta.it>
2026-05-26 16:50:08 +0200

    upgrade conf

--- resources/js/components/WaveformPlayer.vue
+++ resources/js/components/WaveformPlayer.vue
@@ -92,6 +92,8 @@ const initWaveSurfer = () => {
     cursorColor: '#C4E0E9',
     cursorWidth: 2,
     height: 80,
+    mediaControls: true,
+    backend: 'MediaElement',
     ...props.options,
     url: props.url,
     plugins: plugins
