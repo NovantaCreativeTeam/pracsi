dea00680e2ee3265cf72c76ec88accbaed756aa6
Lorenzo Minciotti <lorenzo.minciotti@novanta.it>
2026-05-27 14:43:11 +0200

    Added force to https on production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         // SPA authenticate with session cookie, third parties authenticate with tokens
         $middleware->statefulApi();
     })
